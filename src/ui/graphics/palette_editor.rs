9dcfe34bdf5dfee2c953a0d1d7e93b5f13f1e4d2
RobDavenport <zerve0@hotmail.com>
2022-04-27 01:24:45 +0900

    adjust columns to groups

--- src/ui/graphics/palette_editor.rs
+++ src/ui/graphics/palette_editor.rs
@@ -11,36 +11,49 @@ impl Default for PaletteEditor {
 
 impl PaletteEditor {
     pub fn draw(&mut self, ui: &mut Ui) {
-        ui.columns(2, |columns| {
-            self.draw_palette_list(&mut columns[0]);
-            self.draw_right_side(&mut columns[1]);
+        ui.horizontal(|ui| {
+            self.draw_palette_list(ui);
+            self.draw_right_side(ui)
         });
     }
 
     // Draws the left side panel which displays the palette list widget
     // and related buttons
     fn draw_palette_list(&mut self, ui: &mut Ui) {
-        ui.label("Palette List");
+        ui.group(|ui| {
+            ui.label("Palette List");
+        });
     }
 
     // Draws the right side panel which includes palette viewer, color
     // editor, and sprite preview widgets
     fn draw_right_side(&mut self, ui: &mut Ui) {
-        ui.label("Palette Viewer");
+        ui.group(|ui| {
+            ui.vertical(|ui| {
+                ui.label("Palette Viewer");
 
-        //TODO: Draw the palette viewer
-        ui.columns(2, |columns| {
-            self.draw_color_editor(&mut columns[0]);
-            self.draw_sprite_preview(&mut columns[1]);
+                ui.horizontal(|ui| {
+                    self.draw_color_editor(ui);
+                    self.draw_sprite_preview(ui);
+                });
+            });
         });
     }
 
     // Draws the color editor widget
     fn draw_color_editor(&mut self, ui: &mut Ui) {
-        ui.label("Color Editor");
+        ui.group(|ui| {
+            ui.vertical(|ui| {
+                ui.label("Color Editor");
+            });
+        });
     }
 
     fn draw_sprite_preview(&mut self, ui: &mut Ui) {
-        ui.label("Sprite Preview");
+        ui.group(|ui| {
+            ui.vertical(|ui| {
+                ui.label("Sprite Preview");
+            });
+        });
     }
 }
