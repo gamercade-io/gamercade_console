33c2794de78d3f032de451af0ca1daa883aafc33
RobDavenport <zerve0@hotmail.com>
2022-05-20 00:33:43 +0900

    toggle sprite sheet settings editing

--- src/editor_data/editor_sprite_sheet.rs
+++ src/editor_data/editor_sprite_sheet.rs
@@ -1,7 +1,7 @@
 use gamercade_core::SpriteSheet;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default, PartialEq, Eq)]
 pub struct EditorSpriteSheet {
     pub name: String,
     pub sprite_sheet: SpriteSheet,
--- src/ui/graphics/sprite_sheet_editor_tab/sheet_settings.rs
+++ src/ui/graphics/sprite_sheet_editor_tab/sheet_settings.rs
@@ -1,50 +1,106 @@
 use std::fmt::Write;
 use std::{fmt::Display, str::FromStr};
 
-use egui::{Slider, TextEdit, Ui};
+use egui::{Button, Slider, TextEdit, Ui};
 
 use crate::editor_data::EditorSpriteSheet;
 
 #[derive(Debug, Clone, Default)]
 pub struct SheetSettings {
+    editable: bool,
     buffer: String,
+    preview: EditorSpriteSheet,
 }
 
 impl SheetSettings {
     pub fn draw(&mut self, ui: &mut Ui, sheet: &mut EditorSpriteSheet, palette_count: u8) {
-        //TODO
+        if !self.editable && self.preview != *sheet {
+            self.preview = sheet.clone();
+        }
+
         ui.group(|ui| {
             ui.label("Sprite Sheet Settings");
 
             ui.horizontal(|ui| {
-                self.entry("Name", ui, &mut sheet.name);
+                entry(
+                    &mut self.buffer,
+                    self.editable,
+                    "Name",
+                    ui,
+                    &mut self.preview.name,
+                );
 
                 ui.label("Default Palette");
-                ui.add(Slider::new(
-                    &mut sheet.sprite_sheet.default_palette.0,
-                    0..=palette_count - 1,
-                ))
+                ui.add_enabled(
+                    self.editable,
+                    Slider::new(
+                        &mut self.preview.sprite_sheet.default_palette.0,
+                        0..=palette_count - 1,
+                    ),
+                )
             });
 
             ui.horizontal(|ui| {
-                self.entry("Width", ui, &mut sheet.sprite_sheet.width);
-                self.entry("Height", ui, &mut sheet.sprite_sheet.height);
+                entry(
+                    &mut self.buffer,
+                    self.editable,
+                    "Width",
+                    ui,
+                    &mut self.preview.sprite_sheet.width,
+                );
+                entry(
+                    &mut self.buffer,
+                    self.editable,
+                    "Height",
+                    ui,
+                    &mut self.preview.sprite_sheet.height,
+                );
+            });
+
+            ui.horizontal(|ui| {
+                if ui
+                    .add_enabled(!self.editable, Button::new("Edit"))
+                    .clicked()
+                {
+                    self.editable = true;
+                }
+
+                if ui
+                    .add_enabled(self.editable, Button::new("Apply"))
+                    .clicked()
+                {
+                    *sheet = self.preview.clone();
+                    self.editable = false;
+                }
+
+                if ui
+                    .add_enabled(self.editable, Button::new("Cancel"))
+                    .clicked()
+                {
+                    self.editable = false;
+                }
             })
         });
     }
+}
 
-    fn entry<T: FromStr + Display>(&mut self, label: &'static str, ui: &mut Ui, value: &mut T) {
-        ui.label(label);
+fn entry<T: FromStr + Display>(
+    buffer: &mut String,
+    editable: bool,
+    label: &'static str,
+    ui: &mut Ui,
+    value: &mut T,
+) {
+    ui.label(label);
 
-        self.buffer.clear();
-        write!(&mut self.buffer, "{}", value).unwrap();
-        let widget = TextEdit::singleline(&mut self.buffer);
-        let response = ui.add(widget);
+    buffer.clear();
+    write!(buffer, "{}", value).unwrap();
+    let widget = TextEdit::singleline(buffer);
+    let response = ui.add_enabled(editable, widget);
 
-        if response.changed() {
-            if let Ok(new_val) = self.buffer.parse() {
-                *value = new_val
-            }
+    if response.changed() {
+        if let Ok(new_val) = buffer.parse() {
+            *value = new_val
         }
     }
 }
