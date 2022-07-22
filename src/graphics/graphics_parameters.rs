a7182e1ee2af44e0233a82a8331d1c3eaf427215
RobDavenport <zerve0@hotmail.com>
2022-07-22 18:39:29 +0900

    adjust params to use newtypes

--- src/graphics/graphics_parameters.rs
+++ src/graphics/graphics_parameters.rs
@@ -1,5 +1,7 @@
 use std::mem::transmute;
 
+use crate::{ColorIndex, PaletteIndex, SpriteIndex, SpriteSheetIndex};
+
 const PALETTE_POSITION: u32 = 0;
 const SPRITE_SHEET_POSITION: u32 = 8;
 const SPRITE_INDEX_POSITION: u32 = 16;
@@ -12,33 +14,33 @@ const COLOR_MASK: u32 = MASK >> 2; // 6 bits
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub struct GraphicsParameters {
-    pub palette_index: u8,
-    pub sprite_sheet_index: u8,
-    pub sprite_index: u8,
-    pub color_index: u8,
+    pub palette_index: PaletteIndex,
+    pub sprite_sheet_index: SpriteSheetIndex,
+    pub sprite_index: SpriteIndex,
+    pub color_index: ColorIndex,
     pub flip_x: bool,
     pub flip_y: bool,
 }
 
 impl GraphicsParameters {
     pub fn palette_index(mut self, palette_index: u8) -> Self {
-        self.palette_index = palette_index;
+        self.palette_index = PaletteIndex(palette_index);
         self
     }
 
     pub fn sprite_sheet_index(mut self, sprite_sheet_index: u8) -> Self {
-        self.sprite_sheet_index = sprite_sheet_index;
+        self.sprite_sheet_index = SpriteSheetIndex(sprite_sheet_index);
         self
     }
 
     pub fn sprite_index(mut self, sprite_index: u8) -> Self {
-        self.sprite_index = sprite_index;
+        self.sprite_index = SpriteIndex(sprite_index);
         self
     }
 
     pub fn color_index(mut self, color_index: u8) -> Self {
         assert!(color_index < 64);
-        self.color_index = color_index;
+        self.color_index = ColorIndex(color_index);
         self
     }
 
@@ -57,10 +59,10 @@ impl From<GraphicsParameters> for u32 {
     fn from(gp: GraphicsParameters) -> Self {
         let mut out = 0;
 
-        out |= (gp.palette_index as u32) << PALETTE_POSITION;
-        out |= (gp.sprite_sheet_index as u32) << SPRITE_SHEET_POSITION;
-        out |= (gp.sprite_index as u32) << SPRITE_INDEX_POSITION;
-        out |= (gp.color_index as u32) << COLOR_INDEX_POSITION;
+        out |= (gp.palette_index.0 as u32) << PALETTE_POSITION;
+        out |= (gp.sprite_sheet_index.0 as u32) << SPRITE_SHEET_POSITION;
+        out |= (gp.sprite_index.0 as u32) << SPRITE_INDEX_POSITION;
+        out |= (gp.color_index.0 as u32) << COLOR_INDEX_POSITION;
         out |= (gp.flip_x as u32) << FLIP_X_POSITION;
         out |= (gp.flip_y as u32) << FLIP_Y_POSITION;
 
@@ -77,12 +79,18 @@ impl From<GraphicsParameters> for i32 {
 impl From<u32> for GraphicsParameters {
     fn from(bits: u32) -> Self {
         Self {
-            palette_index: ((bits & MASK << PALETTE_POSITION) >> PALETTE_POSITION) as u8,
-            sprite_sheet_index: ((bits & MASK << SPRITE_SHEET_POSITION) >> SPRITE_SHEET_POSITION)
-                as u8,
-            sprite_index: ((bits & MASK << SPRITE_INDEX_POSITION) >> SPRITE_INDEX_POSITION) as u8,
-            color_index: ((bits & COLOR_MASK << COLOR_INDEX_POSITION) >> COLOR_INDEX_POSITION)
-                as u8,
+            palette_index: PaletteIndex(
+                ((bits & MASK << PALETTE_POSITION) >> PALETTE_POSITION) as u8,
+            ),
+            sprite_sheet_index: SpriteSheetIndex(
+                ((bits & MASK << SPRITE_SHEET_POSITION) >> SPRITE_SHEET_POSITION) as u8,
+            ),
+            sprite_index: SpriteIndex(
+                ((bits & MASK << SPRITE_INDEX_POSITION) >> SPRITE_INDEX_POSITION) as u8,
+            ),
+            color_index: ColorIndex(
+                ((bits & COLOR_MASK << COLOR_INDEX_POSITION) >> COLOR_INDEX_POSITION) as u8,
+            ),
             flip_x: bits & 1 << FLIP_X_POSITION != 0,
             flip_y: bits & 1 << FLIP_Y_POSITION != 0,
         }
@@ -99,42 +107,42 @@ impl From<i32> for GraphicsParameters {
 fn test_graphics_parameters() {
     let params = [
         GraphicsParameters {
-            palette_index: 127,
-            sprite_sheet_index: 255,
-            sprite_index: 92,
-            color_index: 43,
+            palette_index: PaletteIndex(127),
+            sprite_sheet_index: SpriteSheetIndex(255),
+            sprite_index: SpriteIndex(92),
+            color_index: ColorIndex(43),
             flip_x: true,
             flip_y: false,
         },
         GraphicsParameters {
-            palette_index: 43,
-            sprite_sheet_index: 0,
-            sprite_index: 127,
-            color_index: 62,
+            palette_index: PaletteIndex(43),
+            sprite_sheet_index: SpriteSheetIndex(0),
+            sprite_index: SpriteIndex(127),
+            color_index: ColorIndex(62),
             flip_x: false,
             flip_y: true,
         },
         GraphicsParameters {
-            palette_index: 255,
-            sprite_sheet_index: 255,
-            sprite_index: 255,
-            color_index: 63,
+            palette_index: PaletteIndex(255),
+            sprite_sheet_index: SpriteSheetIndex(255),
+            sprite_index: SpriteIndex(255),
+            color_index: ColorIndex(63),
             flip_x: true,
             flip_y: true,
         },
         GraphicsParameters {
-            palette_index: 1,
-            sprite_sheet_index: 1,
-            sprite_index: 1,
-            color_index: 1,
+            palette_index: PaletteIndex(1),
+            sprite_sheet_index: SpriteSheetIndex(1),
+            sprite_index: SpriteIndex(1),
+            color_index: ColorIndex(1),
             flip_x: false,
             flip_y: false,
         },
         GraphicsParameters {
-            palette_index: 0,
-            sprite_sheet_index: 0,
-            sprite_index: 0,
-            color_index: 0,
+            palette_index: PaletteIndex(0),
+            sprite_sheet_index: SpriteSheetIndex(0),
+            sprite_index: SpriteIndex(0),
+            color_index: ColorIndex(0),
             flip_x: false,
             flip_y: false,
         },
