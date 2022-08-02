f4581a36155cb5b8a83083588a3f193f5471a4a9
RobDavenport <zerve0@hotmail.com>
2022-08-03 03:16:43 +0900

    move prelude to top level

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -5,16 +5,6 @@ pub mod multiplayer;
 pub mod random;
 pub mod text;
 
-mod prelude {
-    pub use super::data::*;
-    pub use super::draw::*;
-    pub use super::input::*;
-    pub use super::multiplayer::*;
-    pub use super::random::*;
-    pub use super::text::*;
-    pub use gamercade_core::GraphicsParameters;
-}
-
 pub(crate) fn i32_bool_to_option(val: i32) -> Option<bool> {
     match val {
         0 => Some(false),
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,13 @@ pub mod raw;
 
 mod api;
 pub use api::*;
+
+pub mod prelude {
+    pub use super::data::*;
+    pub use super::draw::*;
+    pub use super::input::*;
+    pub use super::multiplayer::*;
+    pub use super::random::*;
+    pub use super::text::*;
+    pub use gamercade_core::GraphicsParameters;
+}
