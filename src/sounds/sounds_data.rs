630af27f79c52e96c29dc3e8fadb30692c2a6316
RobDavenport <zerve0@hotmail.com>
2022-08-02 22:01:12 +0900

    add licences

--- src/sounds/sounds_data.rs
+++ src/sounds/sounds_data.rs
@@ -1,10 +1,4 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct SoundsData {
-    //TODO: This
-    //patches: Box<[Patch]>,
-    //sequences: Box<[Sequence]>
-
-    // How to handle samples?
-}
+pub struct SoundsData {}
