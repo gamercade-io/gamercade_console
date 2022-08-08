da28da8bc6ec172b4ea86950193c26d1b27d79ec
RobDavenport <zerve0@hotmail.com>
2022-08-09 04:59:56 +0900

    add more algorithms

--- src/fm/algorithm.rs
+++ src/fm/algorithm.rs
@@ -4,14 +4,25 @@ pub enum ModulatedBy {
     None,
     Single(usize),
     Double(usize, usize),
+    Triple(usize, usize, usize),
 }
 
 #[derive(PartialEq, Copy, Clone, Debug, Default)]
 pub struct Algorithm(pub u8);
 
+// These are similar to those found on the Dirtywave m8
 impl Algorithm {
+    pub const fn min() -> u8 {
+        0
+    }
+
+    pub const fn max() -> u8 {
+        11
+    }
+
     pub fn get_definition(self) -> &'static AlgorithmDefinition {
         match self.0 {
+            // A > B > C > D
             0 => &AlgorithmDefinition {
                 carriers: [false, false, false, true],
                 modulators: [
@@ -20,6 +31,8 @@ impl Algorithm {
                     ModulatedBy::Single(2),
                 ],
             },
+
+            // [A + B] > C > D
             1 => &AlgorithmDefinition {
                 carriers: [false, false, false, true],
                 modulators: [
@@ -28,15 +41,9 @@ impl Algorithm {
                     ModulatedBy::Single(2),
                 ],
             },
+
+            // [[A > B] + C] > D
             2 => &AlgorithmDefinition {
-                carriers: [false, false, false, true],
-                modulators: [
-                    ModulatedBy::None,
-                    ModulatedBy::Single(1),
-                    ModulatedBy::Single(2),
-                ],
-            },
-            3 => &AlgorithmDefinition {
                 carriers: [false, false, false, true],
                 modulators: [
                     ModulatedBy::Single(0),
@@ -44,7 +51,49 @@ impl Algorithm {
                     ModulatedBy::Double(1, 2),
                 ],
             },
+
+            // [[A > B] + [A > C]] > D
+            3 => &AlgorithmDefinition {
+                carriers: [false, false, false, true],
+                modulators: [
+                    ModulatedBy::Single(0),
+                    ModulatedBy::Single(0),
+                    ModulatedBy::Double(1, 2),
+                ],
+            },
+
+            // [A + B + C] > D
             4 => &AlgorithmDefinition {
+                carriers: [false, false, false, true],
+                modulators: [
+                    ModulatedBy::None,
+                    ModulatedBy::None,
+                    ModulatedBy::Triple(0, 1, 2),
+                ],
+            },
+
+            // [A > B > C] + D
+            5 => &AlgorithmDefinition {
+                carriers: [false, false, true, true],
+                modulators: [
+                    ModulatedBy::Single(0),
+                    ModulatedBy::Single(1),
+                    ModulatedBy::None,
+                ],
+            },
+
+            // [A > B > C] + [A > B > D]
+            6 => &AlgorithmDefinition {
+                carriers: [false, false, true, true],
+                modulators: [
+                    ModulatedBy::Single(0),
+                    ModulatedBy::Single(1),
+                    ModulatedBy::Single(1),
+                ],
+            },
+
+            // [A > B] + [C > D]
+            7 => &AlgorithmDefinition {
                 carriers: [false, true, false, true],
                 modulators: [
                     ModulatedBy::Single(0),
@@ -52,7 +101,9 @@ impl Algorithm {
                     ModulatedBy::Single(2),
                 ],
             },
-            5 => &AlgorithmDefinition {
+
+            // [A > B] + [A > C] + [A > D]
+            8 => &AlgorithmDefinition {
                 carriers: [false, true, true, true],
                 modulators: [
                     ModulatedBy::Single(0),
@@ -60,11 +111,25 @@ impl Algorithm {
                     ModulatedBy::Single(0),
                 ],
             },
-            6 => &AlgorithmDefinition {
+
+            // [A > B] + [A > C] + D
+            9 => &AlgorithmDefinition {
+                carriers: [false, true, true, true],
+                modulators: [
+                    ModulatedBy::Single(0),
+                    ModulatedBy::Single(0),
+                    ModulatedBy::None,
+                ],
+            },
+
+            // [A > B] + C + D
+            10 => &AlgorithmDefinition {
                 carriers: [false, true, true, true],
                 modulators: [ModulatedBy::Single(0), ModulatedBy::None, ModulatedBy::None],
             },
-            7 => &AlgorithmDefinition {
+
+            // A + B + C + D
+            11 => &AlgorithmDefinition {
                 carriers: [true, true, true, true],
                 modulators: [ModulatedBy::None, ModulatedBy::None, ModulatedBy::None],
             },
