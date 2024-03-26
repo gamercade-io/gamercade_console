52623951d469f713ac0e1e9c23111f297ef50780
RobDavenport <zerve0@hotmail.com>
2024-03-26 20:58:04 +0900

    add stuff

--- gamercade_app/src/client_task.rs
+++ gamercade_app/src/client_task.rs
@@ -1,55 +1,55 @@
-use std::{marker::PhantomData, sync::Arc};
+// use std::{marker::PhantomData, sync::Arc};
 
-use tokio::sync::{
-    mpsc::{channel, Sender},
-    Mutex,
-};
+// use tokio::sync::{
+//     mpsc::{channel, Sender},
+//     Mutex,
+// };
 
-pub struct ClientTask<STATE, REQUEST, HANDLER> {
-    pub state: Arc<Mutex<STATE>>,
-    pub sender: Sender<REQUEST>,
-    handler: PhantomData<HANDLER>,
-}
+// pub struct ClientTask<STATE, REQUEST, HANDLER> {
+//     pub state: Arc<Mutex<STATE>>,
+//     pub sender: Sender<REQUEST>,
+//     handler: PhantomData<HANDLER>,
+// }
 
-impl<STATE, REQUEST, HANDLER> Default for ClientTask<STATE, REQUEST, HANDLER>
-where
-    STATE: Default + Send + 'static,
-    REQUEST: Default + Send + 'static,
-    HANDLER: ClientTaskHandler<STATE, REQUEST> + Default + Send + 'static,
-{
-    fn default() -> Self {
-        let state = Arc::new(Mutex::new(STATE::default()));
-        Self {
-            sender: Self::spawn_task(state.clone()),
-            state,
-            handler: PhantomData::default(),
-        }
-    }
-}
+// impl<STATE, REQUEST, HANDLER> Default for ClientTask<STATE, REQUEST, HANDLER>
+// where
+//     STATE: Default + Send + 'static,
+//     REQUEST: Default + Send + 'static,
+//     HANDLER: ClientTaskHandler<STATE, REQUEST> + Default + Send + 'static,
+// {
+//     fn default() -> Self {
+//         let state = Arc::new(Mutex::new(STATE::default()));
+//         Self {
+//             sender: Self::spawn_task(state.clone()),
+//             state,
+//             handler: PhantomData::default(),
+//         }
+//     }
+// }
 
-impl<STATE, REQUEST, HANDLER> ClientTask<STATE, REQUEST, HANDLER>
-where
-    STATE: Send + 'static,
-    REQUEST: Send + 'static,
-    HANDLER: ClientTaskHandler<STATE, REQUEST> + Send,
-{
-    fn spawn_task(state: Arc<Mutex<STATE>>) -> Sender<REQUEST> {
-        let (client_sender, mut receiver) = channel(4);
+// impl<STATE, REQUEST, HANDLER> ClientTask<STATE, REQUEST, HANDLER>
+// where
+//     STATE: Send + 'static,
+//     REQUEST: Send + 'static,
+//     HANDLER: ClientTaskHandler<STATE, REQUEST> + Send,
+// {
+//     fn spawn_task(state: Arc<Mutex<STATE>>) -> Sender<REQUEST> {
+//         let (client_sender, mut receiver) = channel(4);
 
-        tokio::spawn(async move {
-            let state = state.clone();
-            while let Some(request) = receiver.recv().await {
-                HANDLER::handle_request(&state, request).await
-            }
-        });
+//         tokio::spawn(async move {
+//             let state = state.clone();
+//             while let Some(request) = receiver.recv().await {
+//                 HANDLER::handle_request(&state, request).await
+//             }
+//         });
 
-        client_sender
-    }
-}
+//         client_sender
+//     }
+// }
 
-pub trait ClientTaskHandler<STATE, REQUEST> {
-    fn handle_request(
-        state: &Arc<Mutex<STATE>>,
-        request: REQUEST,
-    ) -> impl std::future::Future<Output = ()> + Send;
-}
+// pub trait ClientTaskHandler<STATE, REQUEST> {
+//     fn handle_request(
+//         state: &Arc<Mutex<STATE>>,
+//         request: REQUEST,
+//     ) -> impl std::future::Future<Output = ()> + Send;
+// }
