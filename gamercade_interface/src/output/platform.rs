4998418fdf4e70cf0ec03c27426da6a53bd4aa88
RobDavenport <zerve0@hotmail.com>
2024-04-21 23:43:56 +0900

    remove footprint

--- gamercade_interface/src/output/platform.rs
+++ gamercade_interface/src/output/platform.rs
@@ -24,14 +24,6 @@ pub struct FrontPageResponse {
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct FootprintResponse {
-    #[prost(message, repeated, tag = "1")]
-    pub voted_games: ::prost::alloc::vec::Vec<VotedGamesResponse>,
-    #[prost(message, repeated, tag = "2")]
-    pub editable_games: ::prost::alloc::vec::Vec<EditableGamesResponse>,
-}
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct EditableGamesResponse {
     #[prost(message, repeated, tag = "1")]
     pub editable_games: ::prost::alloc::vec::Vec<EditableGame>,
@@ -243,31 +235,6 @@ pub mod platform_service_client {
                 .insert(GrpcMethod::new("platform.PlatformService", "GetVotedGames"));
             self.inner.unary(req, path, codec).await
         }
-        pub async fn get_footprint(
-            &mut self,
-            request: impl tonic::IntoRequest<super::super::common::Empty>,
-        ) -> std::result::Result<
-            tonic::Response<super::FootprintResponse>,
-            tonic::Status,
-        > {
-            self.inner
-                .ready()
-                .await
-                .map_err(|e| {
-                    tonic::Status::new(
-                        tonic::Code::Unknown,
-                        format!("Service was not ready: {}", e.into()),
-                    )
-                })?;
-            let codec = tonic::codec::ProstCodec::default();
-            let path = http::uri::PathAndQuery::from_static(
-                "/platform.PlatformService/GetFootprint",
-            );
-            let mut req = request.into_request();
-            req.extensions_mut()
-                .insert(GrpcMethod::new("platform.PlatformService", "GetFootprint"));
-            self.inner.unary(req, path, codec).await
-        }
     }
 }
 /// Generated server implementations.
@@ -303,13 +270,6 @@ pub mod platform_service_server {
             request: tonic::Request<super::super::common::Empty>,
         ) -> std::result::Result<
             tonic::Response<super::VotedGamesResponse>,
-            tonic::Status,
-        >;
-        async fn get_footprint(
-            &self,
-            request: tonic::Request<super::super::common::Empty>,
-        ) -> std::result::Result<
-            tonic::Response<super::FootprintResponse>,
             tonic::Status,
         >;
     }
@@ -563,52 +523,6 @@ pub mod platform_service_server {
                     let fut = async move {
                         let inner = inner.0;
                         let method = GetVotedGamesSvc(inner);
-                        let codec = tonic::codec::ProstCodec::default();
-                        let mut grpc = tonic::server::Grpc::new(codec)
-                            .apply_compression_config(
-                                accept_compression_encodings,
-                                send_compression_encodings,
-                            )
-                            .apply_max_message_size_config(
-                                max_decoding_message_size,
-                                max_encoding_message_size,
-                            );
-                        let res = grpc.unary(method, req).await;
-                        Ok(res)
-                    };
-                    Box::pin(fut)
-                }
-                "/platform.PlatformService/GetFootprint" => {
-                    #[allow(non_camel_case_types)]
-                    struct GetFootprintSvc<T: PlatformService>(pub Arc<T>);
-                    impl<
-                        T: PlatformService,
-                    > tonic::server::UnaryService<super::super::common::Empty>
-                    for GetFootprintSvc<T> {
-                        type Response = super::FootprintResponse;
-                        type Future = BoxFuture<
-                            tonic::Response<Self::Response>,
-                            tonic::Status,
-                        >;
-                        fn call(
-                            &mut self,
-                            request: tonic::Request<super::super::common::Empty>,
-                        ) -> Self::Future {
-                            let inner = Arc::clone(&self.0);
-                            let fut = async move {
-                                <T as PlatformService>::get_footprint(&inner, request).await
-                            };
-                            Box::pin(fut)
-                        }
-                    }
-                    let accept_compression_encodings = self.accept_compression_encodings;
-                    let send_compression_encodings = self.send_compression_encodings;
-                    let max_decoding_message_size = self.max_decoding_message_size;
-                    let max_encoding_message_size = self.max_encoding_message_size;
-                    let inner = self.inner.clone();
-                    let fut = async move {
-                        let inner = inner.0;
-                        let method = GetFootprintSvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
