765637c5bdf84d568e5ca5cd053370f2caa1c971
krzysztofwitkowski <kriskw1999@gmail.com>
2024-06-08 18:52:23 +0200

    feat(hex input): added hex input for the colors

--- gamercade_editor/src/ui/graphics/palette_editor_tab/color_editor.rs
+++ gamercade_editor/src/ui/graphics/palette_editor_tab/color_editor.rs
@@ -1,10 +1,11 @@
-use eframe::egui::{Checkbox, Color32, Image, Slider, TextureHandle, Ui, Vec2};
+use eframe::egui::{ Checkbox, Color32, Image, Slider, TextEdit, TextureHandle, Ui, Vec2 };
 use gamercade_core::Color;
 
 #[derive(Clone, Debug, Default)]
 pub struct ColorEditor {
     prev_color: Color,
     pub preview: Color,
+    pub hex_preview: String,
 }
 
 impl ColorEditor {
@@ -13,12 +14,15 @@ impl ColorEditor {
         ui: &mut Ui,
         current_color: &mut Color,
         texture_handle: &TextureHandle,
-        index: usize,
+        index: usize
     ) {
         if self.prev_color != *current_color {
             self.preview = *current_color;
             self.prev_color = *current_color;
+            self.hex_preview = (*current_color).to_hex_string();
         }
+
+        let mut current_hex = (*current_color).to_hex_string();
 
         ui.group(|ui| {
             ui.vertical(|ui| {
@@ -26,17 +30,26 @@ impl ColorEditor {
 
                 ui.label(format!("Color index: {index}"));
 
-                draw_picker(ui, texture_handle, "Current", false, current_color);
-                draw_picker(ui, texture_handle, "Preview", true, &mut self.preview);
+                draw_picker(ui, texture_handle, "Current", false, current_color, &mut current_hex);
+                draw_picker(
+                    ui,
+                    texture_handle,
+                    "Preview",
+                    true,
+                    &mut self.preview,
+                    &mut self.hex_preview
+                );
 
                 ui.horizontal(|ui| {
                     if ui.button("Revert").clicked() {
                         self.preview = *current_color;
+                        self.hex_preview = (*current_color).to_hex_string();
                     }
 
                     if ui.button("Update").clicked() {
                         *current_color = self.preview;
                         self.prev_color = self.preview;
+                        self.hex_preview = current_hex;
                     }
                 })
             });
@@ -50,6 +63,7 @@ fn draw_picker(
     text: &'static str,
     editable: bool,
     color: &mut Color,
+    hex_color: &mut String
 ) {
     ui.group(|ui| {
         ui.label(text);
@@ -60,17 +74,30 @@ fn draw_picker(
             ui.vertical(|ui| {
                 ui.horizontal(|ui| {
                     ui.label("R");
-                    ui.add_enabled(editable, Slider::new(&mut color.r, 0..=255));
+                    if ui.add_enabled(editable, Slider::new(&mut color.r, 0..=255)).changed() {
+                        *hex_color = color.to_hex_string();
+                    }
                 });
 
                 ui.horizontal(|ui| {
                     ui.label("G");
-                    ui.add_enabled(editable, Slider::new(&mut color.g, 0..=255));
+                    if ui.add_enabled(editable, Slider::new(&mut color.g, 0..=255)).changed() {
+                        *hex_color = color.to_hex_string();
+                    }
                 });
 
                 ui.horizontal(|ui| {
                     ui.label("B");
-                    ui.add_enabled(editable, Slider::new(&mut color.b, 0..=255));
+                    if ui.add_enabled(editable, Slider::new(&mut color.b, 0..=255)).changed() {
+                        *hex_color = color.to_hex_string();
+                    }
+                });
+
+                ui.horizontal(|ui| {
+                    ui.label("Hex");
+                    if ui.add_enabled(editable, TextEdit::singleline(hex_color).desired_width(64.0)).changed() {
+                        let _result = color.update_from_hex_string(hex_color);
+                    }
                 });
 
                 ui.horizontal(|ui| {
